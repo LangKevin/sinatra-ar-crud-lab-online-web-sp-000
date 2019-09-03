1c02dcaf935590fe954456dff080c3e89f1ca333
kevin lang <langkevina@gmail.com>
2019-09-03 16:09:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,28 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    @articles = Article.all
+    binding.pry
+    erb :index
+  end
+
+  get '/articles' do
+    @articles = Article.all
+binding.pry
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find[params[:id]]
+    erb :show
+  end
+  post '/articles' do
+    @article = Article.create(params)
+    # binding.pry
+    redirect to "articles/#{@article.id}"
   end
 end
